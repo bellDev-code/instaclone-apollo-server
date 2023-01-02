0478425fc79c087479d883e730351945576915a4
bellDev-code <sumaoo20@naver.com>
2023-01-02 21:58:36 +0900

    Add dotEnv

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import { ApolloServer } from "apollo-server";
 import { typeDefs, resolvers } from "./schema";
+require("dotenv").config();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
+const PORT = process.env.PORT;
+
 server
-  .listen()
-  .then(() => console.log("Server is running on http://localhost:4000/"));
+  .listen(PORT)
+  .then(() => console.log(`Server is running on http://localhost:${PORT}/`));
