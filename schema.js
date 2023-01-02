a7f0dd840c496aa6ea5be67aa94351dca9e7fbef
bellDev-code <sumaoo20@naver.com>
2023-01-02 21:58:21 +0900

    mutaion Error clear

--- schema.js
+++ schema.js
@@ -8,7 +8,7 @@ __dirname 은 현재 실행 중인 폴더 경로
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
 const loadedResolvers = loadFilesSync(
-  `${__dirname}/**/*.{queries, mutations}.js`
+  `${__dirname}/**/*.{queries,mutations}.js`
 );
 
 export const typeDefs = mergeTypeDefs(loadedTypes);
