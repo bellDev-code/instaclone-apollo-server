e7b6b838bb314faa0ffde42abf76d72ec2591a7d
bellDev-code <sumaoo20@naver.com>
2023-01-09 21:40:52 +0900

    Created Account : Add try catch

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -7,32 +7,38 @@ export default {
       _,
       { firstName, lastName, username, email, password }
     ) => {
-      // check if username or email are already on DB.
-      const existingUser = await client.user.findFirst({
-        where: {
-          OR: [
-            {
-              username,
-            },
-            {
-              email,
-            },
-          ],
-        },
-      });
-      console.log(existingUser);
-      // hash password
-      const uglyPassword = await bcrypt.hash(password, 10);
-      // save and return user
-      return client.user.create({
-        data: {
-          username,
-          email,
-          firstName,
-          lastName,
-          password: uglyPassword,
-        },
-      });
+      try {
+        // check if username or email are already on DB.
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                username,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        if (existingUser) {
+          throw new Error("username 또는 email이 존재합니다.");
+        }
+        // hash password
+        const uglyPassword = await bcrypt.hash(password, 10);
+        // save and return user
+        return client.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 };
