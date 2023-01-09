f43795db6627d573fea49f73f9612cecbda8a7b2
bellDev-code <sumaoo20@naver.com>
2023-01-09 22:19:38 +0900

    login mutation & npm install jsonwebtoken

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -40,5 +40,28 @@ export default {
         console.log(error);
       }
     },
+    login: async (_, { username, password }) => {
+      // find user with args.username
+      const user = await client.user.findFirst({
+        where: { username },
+      });
+
+      if (!user) {
+        return {
+          ok: false,
+          error: "유저를 찾지 못하였습니다.",
+        };
+      }
+      // check password with args.password
+      const succesPassword = await bcrypt.compare(password, user.password);
+
+      if (!succesPassword) {
+        return {
+          ok: false,
+          error: "잘못된 패스워드입니다.",
+        };
+      }
+      // issue a token and send it to the user
+    },
   },
 };
