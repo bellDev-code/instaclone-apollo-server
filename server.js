55f007c6c9dbfefd467eef702b8b2ae3b6d40ce9
bellDev-code <sumaoo20@naver.com>
2023-01-10 22:12:53 +0900

    context function : Add getUser

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 import { ApolloServer } from "apollo-server";
 import { typeDefs, resolvers } from "./schema";
+import { getUser } from "./users/users.utils";
 require("dotenv").config();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => {
+  context: async ({ req }) => {
     return {
-      token: req.headers.token,
+      loggedInUser: await getUser(req.headers.token),
     };
   },
 });
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,14 +1,14 @@
 import client from "../../client";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 
 export default {
   Mutation: {
     editProfile: async (
       _,
       { firstName, lastName, username, email, password: newPassword },
-      { token }
+      { loggedInUser }
     ) => {
+      console.log(loggedInUser);
       // 1. prisma에 undefiend를 보내면 DB에 그 값들을 보내지 않는다.
       // 2. password hash : field에 별칭 선언 hashing 해준다.
       let uglyPassword = null;
@@ -16,12 +16,9 @@ export default {
       if (newPassword) {
         uglyPassword = await bcrypt.hash(newPassword, 10);
       }
-      // 3. 유저 update를 token을 활용하여
-      const { id } = await jwt.verify(token, process.env.SECRET_KEY);
-
       const updateUser = await client.user.update({
         where: {
-          id,
+          id: loggedInUser.id,
         },
         data: {
           firstName,
