29a0e23aae45321fe2211991f72b25dea962f288
bellDev-code <sumaoo20@naver.com>
2023-01-21 02:28:35 +0900

    Follow User Muation

---
+++ users/followUser/followUser.resolvers.js
@@ -0,0 +1,32 @@
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+export default {
+  Mutation: {
+    followUser: protectedResolver(async (_, { username }, { loggedInUser }) => {
+      const ok = await client.user.findUnique({
+        where: { username },
+      });
+
+      if (!ok) {
+        return {
+          ok: false,
+          error: "해당 username은 존재하지 않습니다.",
+        };
+      }
+      await client.user.update({
+        where: { id: loggedInUser.id },
+        data: {
+          following: {
+            connect: {
+              username,
+            },
+          },
+        },
+      });
+      return {
+        ok: true,
+      };
+    }),
+  },
+};
