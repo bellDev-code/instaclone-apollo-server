d07b23defcab688a7e80f38c3bbb2eea5abde348
bellDev-code <sumaoo20@naver.com>
2023-01-31 02:18:34 +0900

    searchUsers pagination

--- users/searchUsers/searchUsers.resolvers.js
+++ users/searchUsers/searchUsers.resolvers.js
@@ -3,13 +3,27 @@ import client from "../../client";
 // pagination 구현, 에러처리 구현
 export default {
   Query: {
-    searchUsers: async (_, { keywords }) =>
-      client.user.findMany({
+    searchUsers: async (_, { keywords, lastId }) => {
+      if (keywords.length < 2) {
+        return {
+          ok: false,
+          error: "두 글자 이상으로 검색해주세요.",
+        };
+      }
+      const users = await client.user.findMany({
         where: {
           username: {
             startsWith: keywords.toLowerCase(),
           },
         },
-      }),
+        take: 5,
+        skip: lastId ? 1 : 0,
+        ...(lastId && { cursor: { id: lastId } }),
+      });
+      return {
+        ok: true,
+        users,
+      };
+    },
   },
 };
--- users/searchUsers/searchUsers.typeDefs.js
+++ users/searchUsers/searchUsers.typeDefs.js
@@ -1,7 +1,12 @@
 import { gql } from "apollo-server";
 
 export default gql`
+  type searchUsersResult {
+    ok: Boolean!
+    error: String
+    users: [User]
+  }
   type Query {
-    searchUsers(keywords: String!): [User]
+    searchUsers(keywords: String!, lastId: Int): searchUsersResult
   }
 `;
