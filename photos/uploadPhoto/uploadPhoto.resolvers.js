6891b122ea688d9ce13ad9bc6161249d720e14a8
bellDev-code <sumaoo20@naver.com>
2023-02-04 15:01:37 +0900

    uploadPhoto maked hashtags

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,15 +1,36 @@
+import client from "../../client";
 import { protectedResolver } from "../../users/users.utils";
 
 export default {
   Mutation: {
     uploadPhoto: protectedResolver(
       async (_, { file, caption }, { loggedInUser }) => {
+        let hashtagObj = [];
         if (caption) {
-          // parse caption
-          // get or create Hashtags
+          const hashtags = caption.match(/#[\w]+/g);
+          hashtagObj = hashtags.map((hashtag) => ({
+            where: { hashtag },
+            create: { hashtag },
+          }));
         }
-        // save the photo WITH the parsed hashtags
-        // add the photo to the hashtags
+
+        return client.photo.create({
+          data: {
+            file,
+            caption,
+
+            user: {
+              connect: {
+                id: loggedInUser.id,
+              },
+            },
+            ...(hashtagObj.length > 0 && {
+              hashtags: {
+                connectOrCreate: hashtagObj,
+              },
+            }),
+          },
+        });
       }
     ),
   },
