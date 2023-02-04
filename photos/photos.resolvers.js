c44494a08aa51e01b5642ff7ba90df14def781cc
bellDev-code <sumaoo20@naver.com>
2023-02-04 16:48:57 +0900

    seeHashtag

--- photos/photos.resolvers.js
+++ photos/photos.resolvers.js
@@ -18,4 +18,25 @@ export default {
         },
       }),
   },
+  Hashtag: {
+    photos: ({ id }) => {
+      return client.hashtag
+        .findUnique({
+          where: {
+            id,
+          },
+        })
+        .photos();
+    },
+    totalPhotos: ({ id }) =>
+      client.photo.count({
+        where: {
+          hashtags: {
+            some: {
+              id,
+            },
+          },
+        },
+      }),
+  },
 };
