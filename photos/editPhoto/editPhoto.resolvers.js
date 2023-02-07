3006f9c341dcc5096f454bea7629fbb9a7ba8b21
bellDev-code <sumaoo20@naver.com>
2023-02-07 23:59:07 +0900

    processHashtags function & editPhoto clear

--- photos/editPhoto/editPhoto.resolvers.js
+++ photos/editPhoto/editPhoto.resolvers.js
@@ -1,29 +1,42 @@
 import client from "../../client";
 import { protectedResolver } from "../../users/users.utils";
+import { processHastags } from "../photos.utils";
 
 export default {
   Mutation: {
     editPhoto: protectedResolver(
       async (_, { id, caption }, { loggedInUser }) => {
-        const ok = client.photo.findFirst({
+        const oldPhoto = await client.photo.findFirst({
           where: {
             id,
             userId: loggedInUser.id,
           },
+          include: {
+            hashtags: {
+              select: {
+                hashtag: true,
+              },
+            },
+          },
         });
 
-        if (!ok) {
+        if (!oldPhoto) {
           return {
             ok: false,
             error: "사진을 찾을 수 없습니다.",
           };
         }
+
         const photo = await client.photo.update({
           where: {
             id,
           },
           data: {
             caption,
+            hashtags: {
+              disconnect: oldPhoto.hashtags,
+              connectOrCreate: processHastags(caption),
+            },
           },
         });
       }
