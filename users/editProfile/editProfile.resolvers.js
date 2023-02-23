f4da7b148c9efece70e9ccf446bcdc9cc826caed
bellDev-code <sumaoo20@naver.com>
2023-02-23 19:40:43 +0900

    Add AWS S3 fordername

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,8 +1,7 @@
 import client from "../../client";
 import bcrypt from "bcrypt";
-import { createWriteStream } from "fs";
 import { protectedResolver } from "../users.utils";
-import { uploadPhoto } from "../../shared/shared.utils";
+import { uploadToS3 } from "../../shared/shared.utils";
 
 const resolverFn = async (
   _,
@@ -18,7 +17,7 @@ const resolverFn = async (
   let avatarUrl = null;
 
   if (avatar) {
-    avatarUrl = await uploadPhoto(avatar, loggedInUser.id);
+    avatarUrl = await uploadToS3(avatar, loggedInUser.id, "avartars");
     // const { filename, createReadStream } = await avatar;
     // const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
     // const readStream = createReadStream();
